fe742e53e409f726d9d55c814bf6dcad42be2783
AbelZewdie <Abelmzewdie@gmail.com>
2024-06-28 14:52:17 -0400

    Fixing Auth

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@
 
 "use strict";
 
-const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com";
+const apiBaseURL = "https://microbloglite.us-east-2.elasticbeanstalk.com";
 // const apiBaseURL = "https://microbloglite.herokuapp.com";
 // Backup server:   https://microbloglite.onrender.com
 
